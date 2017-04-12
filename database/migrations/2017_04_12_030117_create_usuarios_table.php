938230ac0662cf652b5ad8107f1606de9a5f28b8
elvaleli2016 <elvaleli@gmail.com>
2017-04-11 22:15:21 -0500

    migrate y model usuarios, administrador, docente, estudiente

---
+++ database/migrations/2017_04_12_030117_create_usuarios_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsuariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->enum('tipo_identificacion',['cedula','estranjera','tarjeta']);
+            $table->string('identificacion')->unique();
+            $table->string('telefono')->nullable();
+            $table->string('correo')->nullable();
+            $table->string('direccion')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
---
+++ database/migrations/2017_04_12_030215_create_administradors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdministradorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('administradors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('usuario');
+            $table->string('contrasena');
+            $table->foreign('id')->references('id')->on('usuarios');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('administradors');
+    }
+}
