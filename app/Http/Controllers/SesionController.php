02ba55c3dccf7d867df63b70d846951c3ca5b4ce
elvaleli2016 <elvaleli@gmail.com>
2017-04-12 13:37:15 -0500

    Control Sesion

--- app/Http/Controllers/SesionController.php
+++ app/Http/Controllers/SesionController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Administrador;
+use App\Docente;
+use App\Usuario;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Redirect;
 
 class SesionController extends Controller
 {
@@ -11,6 +16,31 @@ class SesionController extends Controller
     }
 
     public function validar(Request $request){
+      $user=null;
+      if($request['tipo_cuenta']=="administrador"){
+        $docente=Administrador::where('usuario',$request['usuario'])->where('contrasena',$request['contrasena']);
+        if($docente->count()!=0){
+          $user=$docente->first();
+        }else{
+        return redirect("/")->with("status","Usuario o contraseña incorrectos");
+        }
+
+      }else if($request['tipo_cuenta']=="docente"){
+          $docente=Docente::where('usuario',$request['usuario'])->where('contrasena',$request['contrasena']);
+          if($docente->count()!=0){
+            $user=$docente->first();
+          }else{
+          return redirect("/")->with("status","Usuario o contraseña incorrectos");
+          }
+      }else{
+        return redirect("/")->with("status","Error en los datos");
+      }
+
+      $usuario=Usuario::where('id',$user->id)->first();
+      $user['nombre']=$usuario->nombre;
+      $user['identificacion']=$usuario->identificacion;
+      $user['telefono']=$usuario->telefono;
+      Session::put('users',$user->toArray());
       dd($request->all());
     }
 }
