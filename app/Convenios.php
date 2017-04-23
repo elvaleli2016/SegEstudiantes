a37ef9d0de88d34c27711961eda1cd3df399cf8c
elvaleli2016 <elvaleli@gmail.com>
2017-04-23 16:15:00 -0500

    Correccio del modelo convenio

---
+++ app/Convenios.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Convenios extends Model
+{
+  /**
+   * The database table used by the model.
+   *
+   * @var string
+   */
+  protected $table = 'convenios';
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = ['id', 'n_convenio','representante_emp','representante_uni', 'fecha', 'concepto','descripcion','costo','archivo', 'palabras_clave','fecha_ini','fecha_fin','empresa'];
+
+
+}
