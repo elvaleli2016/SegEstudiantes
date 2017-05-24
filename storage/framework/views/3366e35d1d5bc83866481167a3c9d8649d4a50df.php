c9b96215df525d72c79fcd3af186522bc4c2b158
elvaleli2016 <elvaleli@gmail.com>
2017-05-23 22:02:03 -0500

    storage

--- storage/framework/views/3366e35d1d5bc83866481167a3c9d8649d4a50df.php
+++ storage/framework/views/3366e35d1d5bc83866481167a3c9d8649d4a50df.php
@@ -3,12 +3,10 @@
 <?php $__env->stopSection(); ?>
 
 <?php $__env->startSection('content'); ?>
-    <?php echo $__env->make('app.pasantia.restore.agregar', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+    <?php echo $__env->make('app.pasantia.restore.editar', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
 <?php $__env->stopSection(); ?>
 
 <?php $__env->startSection('script'); ?>
-<script type="text/javascript" src="https://select2.github.io/dist/js/select2.full.js"></script>
-<link href="https://select2.github.io/dist/css/select2.min.css" type="text/css" rel="stylesheet" />
 <script>
 $('#empresa').change(function(){
   var datos="id="+$('#empresa').val();
@@ -33,31 +31,17 @@ $('#empresa').change(function(){
 
   });
 });
-
- $(function(){
-
-
-      var currentDate = new Date();
-
-    $("#fecha_ini").datepicker({
-        format: 'yyyy-mm-dd',
-        maxDate: 0,
-        changeYear: true 
-    }).attr('readonly', 'readonly');
-    $("#fecha_ini").datepicker("setDate", currentDate);
-    $("#fecha_fin").datepicker({
-        format: 'yyyy-mm-dd',
-        maxDate: 0,
-        changeYear: true 
-    }).attr('readonly', 'readonly');
-    $("#fecha_fin").datepicker("setDate", currentDate);
+$('body').on('focus', "input[name='fecha_ini']", function () {
+    $(this).datepicker({
+        autoclose: true
+    });
 });
-
+$('body').on('focus', "input[name='fecha_fin']", function () {
+    $(this).datepicker({
+        autoclose: true
+    });
+});
 </script>
-<script type="text/javascript">
-$(".js-example-basic-multiple").select2();
-</script>
-
 <?php $__env->stopSection(); ?>
 
 <?php echo $__env->make('app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
---
+++ storage/framework/views/384ef15b7e90a2c974f5c2fa3b351c0b7cc211e9.php
@@ -0,0 +1,123 @@
+<div class="container content-prin profile">
+  <div class="row margin-top-10">
+    <div class="headline-center-v2 headline-center-v2-dark margin-bottom-10">
+      <h1 class="shop-h1" style="font-size: 30px;"><b>Registrar Entrega </b></h1>
+      <span class="bordered-icon"><i class="fa fa-newspaper-o" aria-hidden="true"></i></span>
+    </div>
+    <div class="col-md-12">
+      <div class="row equal-height-columns margin-bottom-10">
+        <div class="container">
+          <!-- Registro empresa-->
+
+          <div id="registrar_empresa">
+              <section class="content-header">
+                  <div class="row">
+
+
+                      <div class="col-xs-12" id="msj">
+                          <?php if(isset($msj)): ?>
+                          <div class="alert alert-success alert-dismissible" role="alert" style="margin-bottom : -5px;margin-top : -5px;z-index:2;">
+                              <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                           <?php echo e($msj); ?>
+
+                          </div>
+                          <?php endif; ?>
+
+                      </div>
+                  </div>
+              </section>
+              <section style="padding : 10px 25px 25px 25px;">
+                  <div class="col-md-12">
+                      <form role="form" id="form-convenio" action="/entrega-pasantia" method="POST" enctype="multipart/form-data" autocomplete="off">
+                          <div class="box box-danger">
+                              <input name="_token" type="hidden" value="<?php echo e(csrf_token()); ?>">
+                              <input name="pasantia" type="hidden" value="<?php echo e($pasantia->id); ?>">
+                              <div class="box-body">
+
+
+                                  <div class="row">
+                                    <div class="col-md-3">
+                                        <div class="form-group">
+                                            <label>No covenio</label>
+                                            <input required type="text" disabled="" class="form-control" name="n_convenio" placeholder="<?php echo e($convenio->n_convenio); ?>" value="<?php echo e($convenio->n_convenio); ?>">
+                                        </div>
+                                    </div>
+
+                                    <div class="col-xs-12"></div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Pasantia</label>
+                                            <input type="text" disabled="" class="form-control" name="pasantia-t" value="<?php echo e($pasantia->titulo); ?>" placeholder="<?php echo e($pasantia->titulo); ?>">
+                                        </div>
+                                    </div>
+                                      <div class="col-md-6">
+                                          <div class="form-group">
+                                              <label>Fecha</label>
+
+                                              <div class="input-group date">
+                                                  <div class="input-group-addon">
+                                                      <i class="fa fa-calendar"></i>
+                                                  </div>
+                                                  <input required type="text" class="form-control pull-right" name="fecha_dia" id="fecha_dia"
+                                                         value="">
+                                              </div>
+                                              <!-- /.input group -->
+                                          </div>
+                                      </div>
+                                      <div class="col-md-6">
+                                          <div class="form-group">
+                                              <label for="exampleInputPassword1">Periodo</label>
+                                              <input type="number" class="form-control" name="tipo_periodo"  placeholder="Digita el periodo">
+                                          </div>
+                                      </div>
+                                      <div class="col-md-6">
+                                          <div class="form-group">
+                                              <label for="exampleInputPassword1">Nota</label>
+                                              <input type="number" class="form-control" name="nota"  placeholder="Digita la nota">
+                                          </div>
+                                      </div>
+                                      <div class="col-md-6">
+                                          <div class="form-group">
+                                              <label for="exampleInputPassword1">Descripcion</label>
+                                              <textarea class="form-control" name="descripcion"  placeholder="Descripcion..." ></textarea>
+                                          </div>
+                                      </div>
+
+                                      <div class="col-md-6">
+                                        <label>Archivo</label>
+                                        <div class="form-group">
+                                          <input id="file-1" type="file" name="file" accept="image/png, image/jpeg, application/pdf, application/msword" class="file" multiple=true data-preview-file-type="any">
+                                        </div>
+                                      </div>
+
+                                  </div>
+                              </div>
+
+
+                              <!-- /.box-body -->
+                              <div class="box-footer">
+                                <div class="row">
+                                  <div class="col-md-6">
+                                    <input type="reset" class="btn btn-sm btn-block btn" id="restaurar" value="RESTAURAR" style="">
+                                  </div>
+                                  <div class="col-xs-6">
+                                    <input type="submit" class="btn btn-sm btn-block btn-danger" id="registrar" value="REGISTRAR" style="">
+                                  </div>
+                                </div>
+                              </div>
+                          </div>
+
+                      </form>
+
+                  </div>
+              </section>
+
+          </div>
+
+
+          <!-- Fin registro empresa-->
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
